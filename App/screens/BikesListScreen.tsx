967be316a8929eb7b633785ee7b8bde8369da79e
Jkotoun <47296031+Jkotoun@users.noreply.github.com>
2021-11-13 23:11:14 +0100

    Add files via upload

--- App/screens/BikesListScreen.tsx
+++ App/screens/BikesListScreen.tsx
@@ -5,11 +5,16 @@ import AuthenticatedContext from '../../context';
 import BikeListItem  from '../components/BikeListItem';
 export default function BikesListScreen() {
   const{authenticated,  setAuthenticated} = React.useContext(AuthenticatedContext)
+
+  const info = {"Distance":"548 km", "Ride Time":'36h 18m'}
+  const info2 = {"Distance":"1235 km", "Ride Time":'80h 10m'}
+  const info3 = {"Distance":"2453 km", "Ride Time":'113h 43m'}
+
   return (
     <View style={{ flex: 1, marginTop:25, alignItems: 'center' }}>
-      <BikeListItem name="Canyon grand canyon 8" bikeCategory="MTB hardtail" rideDistance="548" rideTime="36h 18m"></BikeListItem>
-      <BikeListItem name="Specialized" bikeCategory="Road" rideDistance="1235" rideTime="80h 10m" ></BikeListItem>
-      <BikeListItem name="Qayron carma enduro full" bikeCategory="MTB full suspension" rideDistance="2453" rideTime="113h 43m"></BikeListItem>
+      <BikeListItem name="Canyon grand canyon 8" bikeCategory="MTB hardtail" displayInfo={info}  ></BikeListItem>
+      <BikeListItem name="Specialized" bikeCategory="Road"  displayInfo={info2} ></BikeListItem>
+      <BikeListItem name="Qayron carma enduro full" bikeCategory="MTB full suspension"  displayInfo={info3}></BikeListItem>
       <Button onPress={() => setAuthenticated(false)}  title="Logout"></Button>
     </View>
   );
