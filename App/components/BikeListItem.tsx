4c889147f1877be7583193d4ae95d7b1b863c381
Jkotoun <47296031+Jkotoun@users.noreply.github.com>
2021-11-13 23:11:32 +0100

    Add files via upload

--- App/components/BikeListItem.tsx
+++ App/components/BikeListItem.tsx
@@ -1,7 +1,24 @@
 import * as React from 'react';
 import { Text, View, Image } from 'react-native'
 
+// function renderInfo(dataObj)
+// {
+//     console.log(dataObj)
+//     console.log(flexSize)
+//     dataObj.map((prop,value)=>{
+//         console.log(prop + " : " + value)
+//         return (
+//             <View style={{flex:flexSize}}>
+//                 <Text style={{ color: "#F44336", fontSize: 15, fontWeight: 'bold' }}>{value}</Text>
+//                     <Text style={{ color: "#696969" }}>{value}</Text>
+//             </View>
+//         )
+//     })
+// }
+
 export default function BikeListItem(props) {
+    // let InfoViews = renderInfo(props.displayInfo)
+    const flexSize = 1/Object.keys(props.displayInfo).length 
     return (
         <View style={{  width: '90%', backgroundColor: "#FDFDFD", margin: 10, elevation: 2, borderRadius: 7 }}>
             <View style={{flexDirection: "row",}}>
@@ -9,19 +26,24 @@ export default function BikeListItem(props) {
                     <Text style={{ fontSize: 15, fontWeight: "bold" }}>{props.name}</Text>
                     <Text>{props.bikeCategory}</Text>
                 </View>
-                <View style={{ flex: 0.3 }}>
-                    <Image source={require('../assets/images/full_suspension_mtb_icon.png')} style={{ width: 70, height: 70 }} />
+                <View style={{ flex: 0.3 , paddingTop:15 }}>
+                    { props.bikeCategory=="MTB full suspension" && <Image source={require('../assets/images/full_suspension_mtb_icon.png')} style={{ width: 70, height: 41 }} />}
+                    { props.bikeCategory=="MTB hardtail" && <Image source={require('../assets/images/full_suspension_mtb_icon.png')} style={{ width: 70, height: 41 }} />}
+                    { props.bikeCategory=="Road" && <Image source={require('../assets/images/road_icon.png')} style={{ width: 70, height: 41 }} />}
                 </View>
             </View>
             <View style={{ flexDirection: 'row', paddingBottom:15, marginLeft:15 }}>
-                <View style={{ flex: 0.35 }}>
-                    <Text style={{ color: "#F44336", fontSize: 15, fontWeight: 'bold' }}>{props.rideDistance}km</Text>
-                    <Text style={{ color: "#696969" }}>Distance</Text>
-                </View>
-                <View style={{ flex: 0.35 }}>
-                    <Text style={{ color: "#F44336", fontSize: 15, fontWeight: 'bold' }}>{props.rideTime}</Text>
-                    <Text style={{ color: "#696969"}}>Ride Time</Text>
-                </View>
+                {
+                    Object.entries(props.displayInfo).map((prop,value)=>{
+                        return (
+                            <View style={{flex:flexSize}}>
+                                <Text style={{ color: "#F44336", fontSize: 15, fontWeight: 'bold' }}>{prop[1]}</Text>
+                                    <Text style={{ color: "#696969" }}>{prop[0]}</Text>
+                            </View>
+                        )
+                })
+                
+                }
             </View>
         </View>
 
