1c1545ef59c95c067c7845029021f48b090eb04b
Jkotoun <47296031+Jkotoun@users.noreply.github.com>
2021-11-14 14:29:57 +0100

    Add files via upload

--- App/screens/BikesListScreen.tsx
+++ App/screens/BikesListScreen.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import { Text, View, Button } from 'react-native';
 import AuthenticatedContext from '../../context';
-import BikeListItem  from '../components/BikeListItem';
+import CardItem  from '../components/CardItem';
 export default function BikesListScreen() {
   const{authenticated,  setAuthenticated} = React.useContext(AuthenticatedContext)
 
@@ -10,12 +10,17 @@ export default function BikesListScreen() {
   const info2 = {"Distance":"1235 km", "Ride Time":'80h 10m'}
   const info3 = {"Distance":"2453 km", "Ride Time":'113h 43m'}
 
+  const images = {
+    mtb_full: require("../assets/images/full_suspension_mtb_icon.png"),
+    road: require("../assets/images/road_icon.png")
+};
+
   return (
     <View style={{ flex: 1, marginTop:25, alignItems: 'center' }}>
-      <BikeListItem name="Canyon grand canyon 8" bikeCategory="MTB hardtail" displayInfo={info}  ></BikeListItem>
-      <BikeListItem name="Specialized" bikeCategory="Road"  displayInfo={info2} ></BikeListItem>
-      <BikeListItem name="Qayron carma enduro full" bikeCategory="MTB full suspension"  displayInfo={info3}></BikeListItem>
+      <CardItem title="Canyon grand canyon 8" description="MTB hardtail" icon={images.mtb_full}  displayInfo={info}  ></CardItem>
+      <CardItem title="Specialized" description="Road"  displayInfo={info2} icon={images.road}></CardItem>
+      <CardItem title="Qayron carma enduro full" description="MTB full suspension"  displayInfo={info3} icon={images.mtb_full}></CardItem>
       <Button onPress={() => setAuthenticated(false)}  title="Logout"></Button>
     </View>
   );
-}
+} 
