4d76e042777c0542a574a1720a96df41b5208a1b
Jkotoun <jkotoun58@gmail.com>
2022-07-21 14:24:47 +0200

    config version update

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default{
     expo: {
       name: "Bike Components Manager",
       description: "Bike components manager helps you to track everything about your bike's components",
-      version: "1.0.8", 
+      version: "1.0.9", 
       ios: {
         supportsTablet: true
       },
@@ -13,7 +13,7 @@ export default{
       android: {
         package: "com.bike_components_manager",
         permissions: [],
-        versionCode:25
+        versionCode:26
       },
       splash: {
         image: "./App/assets/splash.png",
